cf5430a19a636ffbe7bef04ac37b9b86762257fd
Bibhas Bhattacharya <bibhas2@gmail.com>
2017-02-17 16:07:35 -0500

    Fix dimension problem in model. Also, create training classification matrix correctly

--- gen_images.py
+++ gen_images.py
@@ -16,12 +16,13 @@ from conv_util import conv_layer, max_pool_layer, weight_variable, bias_variable
 IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 64
 ALLOWED_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+INPUT_CHAR_COUNT = 6 #How many chars in a sample?
 
 def gen_images(sampleCount):
     font = ImageFont.truetype("fake receipt.ttf", 20)
 
     imageResult = np.zeros((sampleCount, IMAGE_HEIGHT, IMAGE_WIDTH, 3))
-    classResult = np.zeros((sampleCount, len(ALLOWED_CHARS)))
+    classResult = np.zeros((sampleCount, INPUT_CHAR_COUNT * len(ALLOWED_CHARS)))
     stringResult = []
 
     for i in range(0, sampleCount):
@@ -29,11 +30,20 @@ def gen_images(sampleCount):
         draw = ImageDraw.Draw(img)
 
         amount = str(random.randrange(100000.00) / 100.0)
-        filledAmount = amount.zfill(len(ALLOWED_CHARS))
+        filledAmount = amount.zfill(INPUT_CHAR_COUNT)
         draw.text((20,20), filledAmount, (255, 255, 255), font=font)
 
         imageResult[i] = np.asarray(img) 
-        classResult[i] = np.fromstring(filledAmount, np.int8)
+
+        thisClass = np.zeros((INPUT_CHAR_COUNT, len(ALLOWED_CHARS)))
+        charList = list(filledAmount)
+
+        for (charPosition, char) in enumerate(charList):
+            charClass = ALLOWED_CHARS.index(char)
+            thisClass[charPosition, charClass] = 1.0
+
+        #Unroll the class matrix
+        classResult[i] = thisClass.reshape(INPUT_CHAR_COUNT * len(ALLOWED_CHARS))
         #img.save(filledAmount + ".png")
         stringResult.append(filledAmount)
 
@@ -88,7 +98,7 @@ def build_model_tf():
     #Build the output layer. One neuron per class. 
     #For each letter position we need probability of each letter type.
     #Number of neurons = number of classes = (letter positions) * (number of possible letters)
-    numNeurons = len(ALLOWED_CHARS) * len(ALLOWED_CHARS)
+    numNeurons = INPUT_CHAR_COUNT * len(ALLOWED_CHARS)
     #Dimension of weight matrix = number of neurons from previous layer X number of classes
     previousNumNeurons = network.get_shape()[1].value
     Wo = weight_variable([previousNumNeurons, numNeurons])
@@ -130,12 +140,14 @@ def predict():
     print validateClasses
     print p
 
-network = build_model_tf()
-print network.get_shape()
+#network = build_model_tf()
+#print network.get_shape()
 
 #train()
 # predict()
-#trainingImages, trainingClasses, trainingStringResults = gen_images(2)
+trainingImages, trainingClasses, trainingStringResults = gen_images(1)
+print trainingClasses[0]
+
 #plt.imshow(trainingImages[0])
 # pil_im = Image.open('824.65.png', 'r')
 # plt.imshow(np.asarray(pil_im))
