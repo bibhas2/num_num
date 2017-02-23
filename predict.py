f1d82cfa24f326437008b3a92cca1ce33a794c6a
Bibhas Bhattacharya <bibhas2@gmail.com>
2017-02-22 20:01:14 -0500

    Increase batch count for training.

--- predict.py
+++ predict.py
@@ -11,12 +11,14 @@ sess.run(init)
 saver = tf.train.Saver()
 saver.restore(sess, "./model.ckpt")
 
-test_X, test_Y = gen_images(2, True)
+test_X, test_Y = gen_images(100)
 
+print "Generated test images. Getting predictions."
 predictions = sess.run(Y, {X: test_X})
 
+print "Calculating accuracy."
 a = sess.run(accuracy, {X: test_X, Y_: test_Y})
 print("Test accuracy:" + str(a))
 
-print np.argmax(predictions, 1)
-print np.argmax(test_Y, 1)
+# print np.argmax(predictions, 1)
+# print np.argmax(test_Y, 1)
